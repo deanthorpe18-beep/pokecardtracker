358ec1ad4fc6409413e22c50d6511c5517e71b14
deanthorpe18-beep <deanthorpe18@googlemail.com>
2026-05-04 19:33:49 +0100

    Create market-page.js

---
+++ market-page.js
@@ -0,0 +1,8 @@
+export default function Market() {
+  return (
+    <div style={{ padding: 20, color: "white" }}>
+      <h1>💰 Market</h1>
+      <p>Pokémon card pricing section coming soon.</p>
+    </div>
+  );
+}
