c948cc9efb520a72b3283b1453238c9966afe1b2
deanthorpe18-beep <deanthorpe18@googlemail.com>
2026-05-04 19:35:35 +0100

    Delete market-page.js

--- market-page.js
+++
@@ -1,8 +0,0 @@
-export default function Market() {
-  return (
-    <div style={{ padding: 20, color: "white" }}>
-      <h1>💰 Market</h1>
-      <p>Pokémon card pricing section coming soon.</p>
-    </div>
-  );
-}
